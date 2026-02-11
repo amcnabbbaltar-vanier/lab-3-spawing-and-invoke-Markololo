892a1ff9dc49f6405450eb84ad3519d0a13b8326
Markololo <markololo2468@gmail.com>
2026-02-11 10:24:19 -0500

    Finished Lab 3

---
+++ Assets/Script/BulletComponent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class BulletComponent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
